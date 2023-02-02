3aa5517f2f1d391a77557db2090e39bddd082dcc
Piotr-Zacharski <zacharski.p.h@gmail.com>
2023-02-02 15:15:00 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+
+
+export default function Home() {
+  return <main className={styles.main}>
+    <div>
+      <h1>Andiamo APP</h1>
+    </div>
+  </main>;
+}
