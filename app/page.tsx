09693c5eb255e0b43be4d0d50e75cb4c33857e0c
Piotr-Zacharski <62928418+Piotr-Zacharski@users.noreply.github.com>
2023-02-22 10:29:17 +0100

    :lipstick: Logo added

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,9 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return <main className={styles.main}>
-    <div>
-      <h1>Andiamo APP</h1>
+    <div className="flex flex-row">
+      <img src="/walk.png" alt="Andiamo"/>
+      <h1 className="text-5xl">Andiamo APP</h1>
     </div>
   </main>;
 }
